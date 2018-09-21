1dfffc5733fecbebb1a24f7368fca59de93974ab
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-09-21 22:50:42 +0200

    Refine genFlow install script

--- scripts/setup_genflow.sh
+++ scripts/setup_genflow.sh
@@ -1,10 +1,13 @@
 PLATFORM='linux'
 UNAMESTR=`uname`
 
-if [[ "$UNAMESTR" == 'Linux' ]]; then
+if [ "$UNAMESTR" == 'Linux' ]; then
    PLATFORM='linux'
-elif [[ "$UNAMESTR" == 'Darwin' ]]; then
+elif [ "$UNAMESTR" == 'Darwin' ]; then
    PLATFORM='macos'
 fi
+
+
+echo "Downloading genFlow for platform '$PLATFORM' in vendor/ directory..."
 
 curl -L https://github.com/cristianoc/genFlow/releases/download/v0.8.0/genflow-$PLATFORM.tar.gz | tar xz -C vendor
