796d0a484420e64e45cde92a124e6efd309457c8
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-09-21 22:45:38 +0200

    Add OS flag for genFlow setup script

--- scripts/setup_genflow.sh
+++ scripts/setup_genflow.sh
@@ -1,1 +1,10 @@
-curl -L https://github.com/cristianoc/genFlow/releases/download/v0.8.0/genflow-macos.tar.gz | tar xz -C vendor
+PLATFORM='linux'
+UNAMESTR=`uname`
+
+if [[ "$UNAMESTR" == 'Linux' ]]; then
+   PLATFORM='linux'
+elif [[ "$UNAMESTR" == 'Darwin' ]]; then
+   PLATFORM='macos'
+fi
+
+curl -L https://github.com/cristianoc/genFlow/releases/download/v0.8.0/genflow-$PLATFORM.tar.gz | tar xz -C vendor
