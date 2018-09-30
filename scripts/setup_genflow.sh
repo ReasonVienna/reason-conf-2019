2fce260eaf060fc3cc6ecef24ee7bc5785efa9e8
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-09-30 22:05:47 +0200

    Cleanup genflow setup script

--- scripts/setup_genflow.sh
+++ scripts/setup_genflow.sh
@@ -12,8 +12,5 @@ echo "Downloading genFlow for platform '$PLATFORM' in node_modules/.bin/ directo
 
 curl -L https://github.com/cristianoc/genFlow/releases/download/v0.8.0/genflow-$PLATFORM.tar.gz | tar xz -C node_modules/.bin
 
-chmod u+x node_modules/.bin/genflow.native
-
-echo "Check genflow:"
-ls -l node_modules/.bin/genflow.native
+echo "Test run genflow:"
 node_modules/.bin/genflow.native --help
