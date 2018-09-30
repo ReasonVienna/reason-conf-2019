566d6769a4895dca89538bb221364bce88cc21de
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-09-30 21:57:06 +0200

    Debug genflow installation

--- scripts/setup_genflow.sh
+++ scripts/setup_genflow.sh
@@ -13,3 +13,6 @@ echo "Downloading genFlow for platform '$PLATFORM' in node_modules/.bin/ directo
 curl -L https://github.com/cristianoc/genFlow/releases/download/v0.8.0/genflow-$PLATFORM.tar.gz | tar xz -C node_modules/.bin
 
 chmod u+x node_modules/.bin/genflow.native
+
+echo "Check genflow:"
+ls -l node_modules/.bin/genflow.native
