5104127457df00cae2c78b89f5648e7f7e5f5f63
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-10-01 14:01:32 +0200

    Get rid of unnecessary code

--- components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AnchorHeader } from "./AnchorHeader";
-export { default as Meta } from "./Meta";
-export { default as Footer } from "./Footer";
-export { default as connect } from "./connect";
