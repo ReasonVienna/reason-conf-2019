f0e69d5b31d895e1051e6e89a476971d8d809e72
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-10-20 14:16:34 +0200

    Remove unnecessary bootstrap code

--- antwar.bootstrap.js
+++ antwar.bootstrap.js
@@ -1,32 +1,20 @@
 /* eslint-disable no-console */
-// Needed for JSX compilation to work
-require("@babel/register");
 
 const antwar = require("antwar");
-const request = require("request");
 const antwarConfig = require("./antwar.config.js");
 const environment = process.argv[2];
 
-// Check that the API is up before starting
-request
-  .get(antwarConfig.apiUrl)
-  .on("response", () => {
-    antwar[environment]({
-      environment,
-      antwar: antwarConfig,
-      webpack: require("./webpack.config")
-    })
-      .then(() => {
-        if (environment !== "build") {
-          console.log("Surf to http://localhost:3000");
-        }
-      })
-      .catch(err => {
-        console.error(err);
-
-        process.exit(1);
-      });
+antwar[environment]({
+  environment,
+  antwar: antwarConfig,
+  webpack: require("./webpack.config")
+})
+  .then(() => {
+    if (environment !== "build") {
+      console.log("Surf to http://localhost:3000");
+    }
   })
-  .on("error", function(err) {
+  .catch(err => {
     console.error(err);
+    process.exit(1);
   });
