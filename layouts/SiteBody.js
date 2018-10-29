6ceb026c2f3431476785d296595364d12cfc1e06
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-10-29 18:43:32 +0100

    Remove JS impl of Meta component

--- layouts/SiteBody.js
+++ layouts/SiteBody.js
@@ -1,43 +1,11 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 
-import Helmet, { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider } from "react-helmet-async";
 
 import Layout from "./layout.re.js";
 
 import "../styles/style.scss";
-
-function Meta({
-  siteName = "test",
-  title = "test",
-  description = "test",
-  keywords = []
-}) {
-  return (
-    <Helmet>
-      <link rel="canonical" href="https://react-finland.fi/" />
-      <link rel="icon" type="image/png" href="/favicon.png" />
-
-      <title>test</title>
-      <meta property="og:site_name" content={siteName} />
-      <meta charSet="UTF-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, minimal-ui"
-      />
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords.join(", ")} />
-
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content="/meta-image.png" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:site" content="@ReactFinland" />
-      <meta name="twitter:image" content="./meta-image.png" />
-    </Helmet>
-  );
-}
 
 const SiteBody = ({ children, location, page: { file } }, ctx) => {
   /*
